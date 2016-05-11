a82eb698557a1956c4d33129ccac9d239893fdc4
Mara Karagianni <mara@multiplace.org>
2016-05-11 16:43:59 +0300

    clean athens dir

--- athens/trains/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Timetable
-
-@admin.register(Timetable)
-class TimetableAdmin(admin.ModelAdmin):
-    #form=TimetableForm
-    list_display = ['station']
-
-# Register your models here.
